e5e9bc2e43d1bb159e5f2c5e852a8e311e6b484d
dobeshs <dobeshs@wwu.edu>
2021-10-22 14:21:24 -0700

    making paths make more sense

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, send_file, render_template
 
 app = Flask(__name__)
 
-@app.route('/download')
+@app.route('/')
 def download():
     path = "./static/downloads/cringe.jpg"
     return send_file(path, as_attachment=True)
