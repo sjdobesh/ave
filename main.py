1a128e0b4ffed656892bf59e4374930bfa81836b
dobeshs <dobeshs@wwu.edu>
2021-10-22 14:09:08 -0700

    basic image upload set up

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, flash, render_template, request
+from flask_uploads import IMAGES, UploadSet, configure_uploads
 
 app = Flask(__name__)
+photos = UploadSet("photos", IMAGES);
+app.config["UPLOADED_PHOTOS_DEST"] = "static/uploads"
+app.config["SECRET_KEY"] = os.urandom(24)
+configure_uploads(app, photos)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST' and 'photo' in request.files:
+        photos.save(request.files['photo'])
+        flash("Photo saved succesfully.")
+        return render_template('upload.html')
+    return render_template('upload.html')
