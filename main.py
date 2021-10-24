3eaf409920dd1b8e4f2d02c8c0d467dd0c9257e5
harangn <43688429+harangn@users.noreply.github.com>
2021-10-24 12:14:50 -0700

    escape(filename) so it is markup-safe

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, flash, render_template, request
 from flask_uploads import UploadSet, configure_uploads
 from werkzeug.utils import redirect, secure_filename
+from markupsafe import escape
 
 app = Flask(__name__)
 extensions = ('mp4') #restricting to mp4 for now
@@ -27,4 +28,4 @@ def upload():
 @app.route('/uploads/<filename>')
 def send_uploaded_file(filename=''):
     from flask import send_from_directory
-    return send_from_directory(app.config["UPLOADED_VIDEOS_DEST"], filename)
+    return send_from_directory(app.config["UPLOADED_VIDEOS_DEST"], escape(filename))
