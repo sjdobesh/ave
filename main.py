3b84bc9fed7379fbe9e70f6cfcb86f5c029afd43
Robin Balatbat <64383337+offdragon@users.noreply.github.com>
2021-10-23 21:00:22 -0700

    Uploaded video now displays on main page, had to restrict file typing

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 from flask import Flask, flash, render_template, request
 from flask_uploads import UploadSet, configure_uploads
+from werkzeug.utils import redirect
 
 app = Flask(__name__)
-extensions = ('mp4', 'mov', 'mvk')
-videos = UploadSet("videos", extensions);
+extensions = ('mp4') #restricting to mp4 for now
+videos = UploadSet("videos", extensions)
 app.config["UPLOADED_VIDEOS_DEST"] = "static/uploads"
 app.config["SECRET_KEY"] = os.urandom(24)
 configure_uploads(app, videos)
@@ -17,8 +18,13 @@ def upload():
             videoname = video.filename
             videos.save(video)
             flash("Video " + videoname + " saved successfully.")
-            return render_template('index.html')
+            return render_template('index.html', uploaded_video=videoname)
         except:
             flash("Incorrect file type, try again")
             return render_template('upload.html')
     return render_template('upload.html')
+
+@app.route('/uploads/<filename>')
+def send_uploaded_file(filename=''):
+    from flask import send_from_directory
+    return send_from_directory(app.config["UPLOADED_VIDEOS_DEST"], filename)
