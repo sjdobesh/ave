28222ad320a902d3f27548f301a2a2bee8db46e5
Robin Balatbat <64383337+offdragon@users.noreply.github.com>
2021-10-24 18:35:07 -0700

    Added duration timeline for video player

---
+++ static/scripts/videoPlayer.js
@@ -0,0 +1,55 @@
+// Code from https://freshman.tech/custom-html5-video/
+const video = document.getElementById("videoPlayer");
+const videoControls = document.getElementById("videoControls");
+
+// const videoWorks = !!document.createElement('video').canPlayType;
+// if (videoWorks) {
+//     video.controls = false;
+//     videoControls.classList.remove('hidden');
+// }
+
+// // play button
+// const playButton = document.getElementById("play");
+
+// function togglePlay() {
+//     if (video.paused || video.ended) {
+//         video.play();
+//     } else {
+//         video.pause();
+//     }
+// }
+
+// time-elapsed
+const timeElapsed = document.getElementById('time-elapsed');
+const duration = document.getElementById('duration');
+
+function formatTime(timeInSeconds) {
+    const result = new Date(timeInSeconds * 1000).toISOString().substr(11, 8);
+
+    return {
+        minutes: result.substr(3, 2),
+        seconds: result.substr(6, 2),
+    };
+}
+
+// initializeVideo sets the video duration, and maximum value of the
+// progressBar
+function initializeVideo() {
+    const videoDuration = Math.round(video.duration);
+    const time = formatTime(videoDuration);
+    duration.innerText = `${time.minutes}:${time.seconds}`;
+    duration.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+}
+
+// updateTimeElapsed indicates how far through the video
+// the current playback is
+function updateTimeElapsed() {
+    const time = formatTime(Math.round(video.currentTime));
+    timeElapsed.innerText = `${time.minutes}:${time.seconds}`;
+    timeElapsed.setAttribute('datetime', `${time.minutes}m ${time.seconds}s`)
+
+}
+
+// playButton.addEventListener('click', togglePlay);
+video.addEventListener('loadedmetadata', initializeVideo);
+video.addEventListener('timeupdate', updateTimeElapsed);
