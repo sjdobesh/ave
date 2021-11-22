a25118263cef50ea96704dff20a55733b8bdd528
harangn <43688429+harangn@users.noreply.github.com>
2021-11-21 20:04:02 -0800

    Remove console logs

--- static/scripts/videoUploadForm.js
+++ static/scripts/videoUploadForm.js
@@ -3,18 +3,12 @@ function updateUploadForm() {
     let input = document.getElementById("video");
     let button = document.getElementById("upload-button");
 
-    console.log(label.innerHTML);
-    console.log(input.value);
-
     let fileName = input.value;
     if (fileName.includes("\\")) {
         let reg = /([^\\]+$)/; // Get everything after last slash: https://regex101.com/library/zJ1zK6
         fileName = input.value.match(reg)[0]; // Get everything after last slash from filepath
-        console.log(fileName);
         // TODO not sure how this works on mobile browsers...
     }
-
-    console.log(button.innerHTML);
 
     label.innerHTML = fileName;
     button.innerHTML = "Upload " + fileName;
