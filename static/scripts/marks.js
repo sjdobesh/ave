f00dde3527f4ea868189a368ded23caab95f637c
harangn <43688429+harangn@users.noreply.github.com>
2021-12-02 09:51:53 -0800

    Create input boxes for marker time

---
+++ static/scripts/marks.js
@@ -0,0 +1,13 @@
+function updateMark() {
+    console.log("In updateMark");
+    // if (isNaN(markNumber) || (markNumber != 0 && markNumber != 1)) {
+    //     console.log("Issue with input...");
+    //     return false;
+    // }
+
+    // let formID = "m" + markNumber + "-form"
+    // console.log(formID);
+    // let form = document.forms["m" + markNumber + "-form"];
+
+    return false;
+}
